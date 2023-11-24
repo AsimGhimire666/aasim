62777309d972da031e07e93c229a9d3bbff4523a
Asimghimire <127404348+Asimghimire@users.noreply.github.com>
2023-11-24 12:16:42 +0545

    jfgasflugsad

---
+++ CRUDWithADONet/CRUDWithADONet/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace CRUDWithADONet.Models
+{
+    public class Employee
+    {
+        [key]
+        public int Id { get; set; }
+
+        [DisplayName("First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [DisplayName("LastName")]
+        [Required]
+        public string LastName { get; set; }
+
+        [DisplayName("Date Of Birth")]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+
+}
